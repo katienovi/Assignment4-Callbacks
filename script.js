2f0a29462dd2cfda37314bfea00eaef2ba1a054f
Katie <kalufukatie@gmail.com>
2026-02-28 14:14:25 -0500

    Added text for user and computer throw and constants for js

---
+++ script.js
@@ -0,0 +1,7 @@
+const playerRock = document.querySelector('#rock-img');
+const playerScissors = document.querySelector('#scissors-img');
+const playerPaper = document.querySelector('#paper-img');
+const playerChoices = [playerRock, playerScissors, playerPaper];
+
+const computerChoices = ['rock', 'paper', 'scissors'];
+
